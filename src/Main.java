9b0e3db1a08d09d8fc2d543a62af5df0c61a0ca9
Eugeny Kostylev <HasRButNoC@yandex.ru>
2025-08-02 01:52:20 +0300

    Добавлен менеджер задач с функционалом сохранения и загрузки задач в файл и из файла.

--- src/Main.java
+++ src/Main.java
@@ -1,53 +1,41 @@
 import task.*;
 import manager.*;
 
+import java.io.File;
+import java.io.IOException;
+
 public class Main {
-    private static final TaskManager manager = Managers.getDefault();
+    private static TaskManager firstManager;
+    private static TaskManager secondManager;
+    private static File file;
 
     public static void main(String[] args) {
+        try {
+            file = File.createTempFile("tmpManagerTest", ".txt");
+            firstManager = Managers.getFileBackedManager(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         Task task1 = new Task("Приготовить перекус", "Заварить лимонный чай с печаньками", Status.NEW);
-        manager.createTask(task1);
+        firstManager.createTask(task1);
         Task task2 = new Task("Привести себя в порядок", "Сходить помыться", Status.NEW);
-        manager.createTask(task2);
+        firstManager.createTask(task2);
         Epic epic1 = new Epic("Выйти на прогулку", "Стоять сходить развеяться");
-        manager.createEpic(epic1);
-        Subtask subtask1epic1 = new Subtask(epic1.getId(), "Одеться по погоде", "Нельзя простужаться",
+        firstManager.createEpic(epic1);
+        Epic epic2 = new Epic("Купить билеты", "Купить билеты для поездки на отдых");
+        firstManager.createEpic(epic2);
+        Subtask subtask1epic1 = new Subtask(epic1.getId(), "Посмотреть погоду", "Вдруг будет дождь",
+                Status.DONE);
+        firstManager.createSubtask(subtask1epic1);
+        Subtask subtask2epic1 = new Subtask(epic1.getId(), "Одеться по погоде", "Нельзя простужаться",
                 Status.NEW);
-        manager.createSubtask(subtask1epic1);
-        Subtask subtask2epic1 = new Subtask(epic1.getId(), "Взять наушники",
-                "Без музики гулять скучно", Status.NEW);
-        manager.createSubtask(subtask2epic1);
-        Subtask subtask3epic1 = new Subtask(epic1.getId(), "Побрызгать", "Клещи бунтуют в лесу!",
-                Status.NEW);
-        manager.createSubtask(subtask3epic1);
-        Epic epic2 = new Epic("Покушать", "Накрыть стол для ужина");
-        manager.createEpic(epic2);
-
-
-        manager.getEpic(epic1.getId());
-        manager.getTask(task1.getId());
-        manager.getTask(task2.getId());
-        manager.getSubtask(subtask3epic1.getId());
-        manager.getSubtask(subtask3epic1.getId());
-        manager.getSubtask(subtask1epic1.getId());
-        manager.getEpic(epic2.getId());
-        manager.getSubtask(subtask2epic1.getId());
-        System.out.println(manager.getHistory());
-
-        manager.getTask(task1.getId());
-        manager.getEpic(epic2.getId());
-        manager.getTask(task2.getId());
-        manager.getSubtask(subtask1epic1.getId());
-        manager.getSubtask(subtask2epic1.getId());
-        manager.getEpic(epic1.getId());
-        manager.getSubtask(subtask3epic1.getId());
-        manager.getTask(task2.getId());
-        System.out.println(manager.getHistory());
-
-        manager.removeTask(task2.getId());
-        System.out.println(manager.getHistory());
-
-        manager.removeEpic(epic1.getId());
-        System.out.println(manager.getHistory());
+        firstManager.createSubtask(subtask2epic1);
+        secondManager = Managers.getFileBackedManager(file);
+        secondManager.getTask(task1.getId());
+        secondManager.getTask(task2.getId());
+        secondManager.getEpic(epic1.getId());
+        secondManager.getEpic(epic2.getId());
+        secondManager.getSubtask(subtask1epic1.getId());
+        secondManager.getSubtask(subtask2epic1.getId());
     }
 }
